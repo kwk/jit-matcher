b0fb727e8d1acf8c1f49f91f3b6afda8d0cd6141
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2014-06-27 17:17:20 +0200

    Added the original LICENSE.TXT as CLANG-LICENSE.TXT

--- jit-matcher.cpp
+++ jit-matcher.cpp
@@ -3,7 +3,7 @@
 //                     The LLVM Compiler Infrastructure
 //
 // This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
+// License. See CLANG-LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
 
